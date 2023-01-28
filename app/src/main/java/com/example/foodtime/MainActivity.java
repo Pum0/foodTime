09a3b9e523aa5aed4a40b54e192f54bdd76917c0
Pum0 <daehwan9960@naver.com>
2023-01-28 19:07:26 +0900

    Splash screen 적용 중 실기기 테스트 필요

--- app/src/main/java/com/example/foodtime/MainActivity.java
+++ app/src/main/java/com/example/foodtime/MainActivity.java
@@ -4,6 +4,7 @@ package com.example.foodtime;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.splashscreen.SplashScreen;
 
 import com.naver.maps.map.NaverMapSdk;
 
@@ -11,6 +12,8 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        SplashScreen splashScreen = SplashScreen.installSplashScreen(this);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
