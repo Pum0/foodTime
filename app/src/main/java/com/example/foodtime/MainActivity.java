f1e8fc2316938919730bc336b6ef9bcdf82959a7
Pum0 <daehwan9960@naver.com>
2023-02-08 02:58:51 +0900

    Naver Map 수정 및 현재 위치 정보 표시 뒤로가기 어플 종료 적용

--- app/src/main/java/com/example/foodtime/MainActivity.java
+++ app/src/main/java/com/example/foodtime/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.foodtime;
 
 
+import android.os.Build;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.splashscreen.SplashScreen;
@@ -10,6 +12,8 @@ import com.naver.maps.map.NaverMapSdk;
 
 public class MainActivity extends AppCompatActivity {
 
+    private long pressedTime = 0; //'뒤로가기' 버튼 클릭했을 때의 시간
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         SplashScreen splashScreen = SplashScreen.installSplashScreen(this);
@@ -17,9 +21,51 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        NaverMapSdk.getInstance(this).setClient(
-                new NaverMapSdk.NaverCloudPlatformClient("vherh2ll56"));
+        NaverMapFragment naverFragment = new NaverMapFragment();
+        getSupportFragmentManager().beginTransaction()
+                .add(R.id.main_container, naverFragment).commit();
 
 
     }
+
+    @Override
+    public void onBackPressed() {
+
+        //마지막으로 누른 '뒤로가기' 버튼 클릭 시간이 이전의 '뒤로가기' 버튼 클릭 시간과의 차이가 2초보다 크면
+        if(System.currentTimeMillis() > pressedTime + 2000){
+            //현재 시간을 pressedTime 에 저장
+            pressedTime = System.currentTimeMillis();
+            Toast.makeText(getApplicationContext(),"한번 더 누르면 종료", Toast.LENGTH_SHORT).show();
+        }
+
+        //마지막 '뒤로가기' 버튼 클릭시간이 이전의 '뒤로가기' 버튼 클릭 시간과의 차이가 2초보다 작으면
+        else{
+            Toast.makeText(getApplicationContext(),"종료 완료", Toast.LENGTH_SHORT).show();
+            // 앱 종료
+            exitProgram();
+        }
+    }
+
+
+    private void exitProgram() {
+        // 종료
+
+        // 태스크를 백그라운드로 이동
+        // moveTaskToBack(true);
+
+        if (Build.VERSION.SDK_INT >= 21) {
+            // 액티비티 종료 + 태스크 리스트에서 지우기
+            finishAndRemoveTask();
+        } else {
+            // 액티비티 종료
+            finish();
+        }
+
+        System.exit(0);
+    }
+
 }
+
+
+//        NaverMapSdk.getInstance(this).setClient(
+//                new NaverMapSdk.NaverCloudPlatformClient("vherh2ll56"));
---
+++ app/src/main/java/com/example/foodtime/NaverMapFragment.java
@@ -0,0 +1,143 @@
+package com.example.foodtime;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.naver.maps.map.LocationTrackingMode;
+import com.naver.maps.map.MapView;
+import com.naver.maps.map.NaverMap;
+import com.naver.maps.map.OnMapReadyCallback;
+import com.naver.maps.map.UiSettings;
+import com.naver.maps.map.util.FusedLocationSource;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link NaverMapFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class NaverMapFragment extends Fragment implements OnMapReadyCallback {
+
+    // 지도 객체 변수
+    private MapView mapView;
+
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1000;
+    private FusedLocationSource locationSource;
+    private NaverMap naverMap;
+
+    public NaverMapFragment() { }
+
+    public static NaverMapFragment newInstance() {
+        NaverMapFragment fragment = new NaverMapFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        locationSource =
+                new FusedLocationSource(this, LOCATION_PERMISSION_REQUEST_CODE);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_naver_map, container, false);
+
+        mapView = (MapView) rootView.findViewById(R.id.navermap);
+        mapView.onCreate(savedInstanceState);
+        mapView.getMapAsync(this);
+
+        return rootView;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String[] permissions,  @NonNull int[] grantResults) {
+        if (locationSource.onRequestPermissionsResult(
+                requestCode, permissions, grantResults)) {
+            if (!locationSource.isActivated()) { // 권한 거부됨
+                naverMap.setLocationTrackingMode(LocationTrackingMode.None);
+            }
+            return;
+        }
+        super.onRequestPermissionsResult(
+                requestCode, permissions, grantResults);
+    }
+
+    @Override
+    public void onMapReady(@NonNull NaverMap naverMap) {
+        this.naverMap = naverMap;
+        naverMap.setLocationSource(locationSource);
+        naverMap.setLocationTrackingMode(LocationTrackingMode.Follow);
+
+        uiSetting();
+    }
+
+    private void uiSetting() {
+        // UI 컨트롤 재배치
+        UiSettings uiSettings = naverMap.getUiSettings();
+        uiSettings.setCompassEnabled(false); // 기본값 : true
+        uiSettings.setScaleBarEnabled(false); // 기본값 : true
+        uiSettings.setZoomControlEnabled(false); // 기본값 : true
+        uiSettings.setLocationButtonEnabled(true); // 기본값 : false
+    }
+
+    @Override
+    public void onStart()
+    {
+        String addr;
+
+        super.onStart();
+        mapView.onStart();
+    }
+
+    @Override
+    public void onResume()
+    {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    public void onPause()
+    {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState)
+    {
+        super.onSaveInstanceState(outState);
+        mapView.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public void onStop()
+    {
+        super.onStop();
+        mapView.onStop();
+    }
+
+    @Override
+    public void onDestroyView()
+    {
+        super.onDestroyView();
+        mapView.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory()
+    {
+        super.onLowMemory();
+        mapView.onLowMemory();
+    }
+}
